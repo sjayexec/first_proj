a23fa0180de8c53cc0d352c2c2b3c8c5e8eae126
Gen Eric <genericmyacc007@gmail.com>
2020-10-08 12:37:43 +0530

    Creating In Touch android application - new project start

---
+++ app/src/main/java/com/phoenixfeatherapp/intouch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.phoenixfeatherapp.intouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
